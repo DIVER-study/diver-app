bbd3b971db190ac0dfd4fb4453c041e870dba42f
Kodie <kodii.tulip@gmail.com>
2024-12-05 13:10:52 -0300

    faxina de arquivos e signup

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,9 @@
 export default function ErrorPage() {
-    return <p>Erro, deu algum problema</p>
-  }
+  return (
+    <div className='h-screen content-center w-full'>
+      <span className='bg-red-500/90 rounded-md p-3 mx-auto table text-neutral-50'>
+        Erro, deu algum problema
+      </span>
+    </div>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,28 @@
-import { login } from './actions'
+import { login, signup } from './actions';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-    </form>
-  )
+    <div className='block content-center h-screen'>
+      <form className='flex flex-col max-w-xs mx-auto gap-2 p-4 ring-1 ring-neutral-500 rounded-xl'>
+        <label htmlFor='email'>Email:</label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          required
+          className='bg-neutral-200 p-2 rounded-md'
+        />
+        <label htmlFor='password'>Password:</label>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          required
+          className='bg-neutral-200 p-2 rounded-md'
+        />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign Up</button>
+      </form>
+    </div>
+  );
 }

abd5a8e4da1a3dbf3a34284a404a3d7a0750bc3e
Kodie <kodii.tulip@gmail.com>
2024-12-07 22:41:55 -0300

    atualizando metadata, favicon. testando adiquirir informações de usuário

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-export default function Home() {
-	return (
-		<div className='w-full h-screen content-center'>
-			<div className='mx-auto text-6xl font-jetbrains-mono font-bold text-center'>
-				Hello
-			</div>
-			<div className='mx-auto text-6xl font-poppins font-bold text-center'>
-				World.
-			</div>
-		</div>
-	);
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.auth.getUser();
+
+  return (
+    <div className='w-full h-screen content-center'>
+      <div className='mx-auto text-6xl font-jetbrains-mono font-bold text-center'>
+        Hello
+      </div>
+      <div className='mx-auto text-6xl font-poppins font-bold text-center'>
+        World.
+      </div>
+      <div className='mx-auto text-xl font-poppins font-medium text-orange-400/80 text-center mt-12'>
+        {data.user ? data.user.user_metadata.email : ''}
+      </div>
+    </div>
+  );
 }

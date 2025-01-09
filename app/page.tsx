1c69725eeee489866e7894e2350294b9f811bfad
Kodie <kodii.tulip@gmail.com>
2025-01-09 13:36:58 -0300

    mudança de nome de usuário funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import LogOutButton from '@/components/LogOut';
 import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
 
 export default async function Home() {
   const supabase = await createClient();
@@ -13,8 +14,14 @@ export default async function Home() {
       <div className='mx-auto text-xl font-poppins font-medium text-orange-400/80 text-center mt-12'>
         {data.user && (data.user.user_metadata.display_name || data.user.user_metadata.email)}
       </div>
-      <div className='mx-auto text-xl font-poppins font-medium text-center mt-12'>
+      <div className='mx-auto text-xl font-poppins font-medium text-center mt-12 space-x-4'>
         <LogOutButton />
+        <Link
+          href='/settings'
+          className='p-2 mt-4 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500'
+        >
+          Configurações de Usuário
+        </Link>
       </div>
     </div>
   );

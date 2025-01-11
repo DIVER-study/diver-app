423ec6c09958c1bfd8f584ca86f93bb5a6df0e65
pvictor1206 <paulomagalhaes1206@gmail.com>
2025-01-11 19:35:51 -0300

    conclusão RF 029

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -3,6 +3,8 @@
 import { createClient } from '@/utils/supabase/server';
 import { type AuthResponse, type AuthTokenResponsePassword } from '@supabase/supabase-js';
 
+import { User } from '../../models/userModel';
+
 // TODO: utilizando um tipo temporário enquanto não temos um UserSchema
 export type UserData = {
   email: string;
@@ -15,6 +17,11 @@ export async function signInWithPassword(userData: UserData): Promise<AuthTokenR
 }
 
 export async function signUpWithPassword(userData: UserData & { options?: object }): Promise<AuthResponse> {
-  const supabase = await createClient();
-  return supabase.auth.signUp(userData);
+  if (!User.isValidEmail(userData.email)) {
+    throw new Error("O e-mail deve terminar com '@alu.ufc.br'");
+  }else{
+    const supabase = await createClient();
+    return supabase.auth.signUp(userData);
+  }
+
 }

401bebf4d00d79e491522e29148ed4027760fc82
Kodie <kodii.tulip@gmail.com>
2025-01-19 14:54:32 -0300

    fix & style: adaptando página biblioteca

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,4 +1,5 @@
 import { SideBar } from '@/components/SideBar';
+import { LibrarySection } from '@/components/LibraryComponents';
 
 /*
  * TODO: pegar do banco de dados as informações como:
@@ -10,37 +11,9 @@ export default function LibraryPage() {
     <div className='flex h-screen'>
       <SideBar activeTab='biblioteca' />
       <main className='flex-1 h-full space-y-8 overflow-y-scroll'>
-        <section className='space-y-4 mx-auto max-w-screen-md'>
-          <h2>Behaviorismo</h2>
-          <hr className='bg-neutrals-700' />
-          <div className='flex gap-4 overflow-x-scroll w-min'>
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-          </div>
-        </section>
-        <section className='space-y-4 mx-auto max-w-screen-md overflow-hidden'>
-          <h2>Gestalt</h2>
-          <hr className='bg-neutrals-700' />
-          <div className='flex gap-4 overflow-x-scroll w-min'>
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-          </div>
-        </section>
-        <section className='space-y-4 mx-auto max-w-screen-md'>
-          <h2>Teoria Sociocultural</h2>
-          <hr className='bg-neutrals-700' />
-          <div className='flex gap-4 overflow-x-scroll w-min'>
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-            <div className='bg-neutral-400 size-40' />
-          </div>
-        </section>
+        <LibrarySection sectionName='Behaviorismo' />
+        <LibrarySection sectionName='Gestalt' />
+        <LibrarySection sectionName='Teoria Sociocultural' />
       </main>
     </div>
   );
---
+++ components/LibraryComponents.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+type SectionProps = {
+  sectionName: string;
+};
+
+export function LibrarySection({ sectionName }: SectionProps) {
+  return (
+    <section className='space-y-4 mx-auto max-w-screen-md'>
+      <h2>{sectionName}</h2>
+      <hr className='bg-neutrals-700' />
+      <div className='grid grid-flow-col auto-cols-[100%] gap-2 overflow-x-scroll scroll-smooth snap-x snap-proximity'>
+        {new Array(4).fill('').map((_, idx) => (
+          <div
+            key={idx}
+            className='grid grid-flow-col auto-cols-[1fr] gap-2 relative snap-start'
+          >
+            {new Array(4).fill('').map((_, index) => (
+              <div
+                className='w-32 aspect-square bg-black rounded-lg'
+                key={index}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}

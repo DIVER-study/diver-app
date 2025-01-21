5c9f99f1fe1d05fafd776355e9208be72949fba2
Kodie <kodii.tulip@gmail.com>
2025-01-21 15:18:14 -0300

    feat!: opção de participar do ranking
    usuários vão precisar aceitar participar do ranking antes de verem a
    página de ranking
    
    BREAKING CHANGE: página de ranking foi movida para um componente
    cliente-side

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -1,40 +1,11 @@
 import { SideBar } from '@/components/SideBar';
-import { UserRankingItem } from '@/components/UserRankingItem';
-import { createClient } from '@/utils/supabase/server';
+import ClientRankingPage from './clientPage';
 
-export default async function RankingPage() {
-  const supabase = await createClient();
-
-  const { data: users, error } = await supabase.from('profiles').select('*');
-
-  if (error) console.error(`Erro: ${error.message}`);
-
+export default function RankingPage() {
   return (
     <div className='flex h-screen'>
       <SideBar activeTab='ranking' />
-      <main className='flex-1 overflow-y-scroll content-center'>
-        <table className='border-collapse my-4 min-w-[400px] [&_th]:p-4 [&_td]:p-4 mx-auto w-full max-w-[600px]'>
-          <caption>Ranking</caption>
-          <thead>
-            <tr className='bg-emerald-600 text-white text-left font-bold'>
-              <th scope='col'>Posição</th>
-              <th scope='col'>Usuário</th>
-              <th scope='col'>Experiência</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users?.map(({ avatar_url, display_name }, index) => (
-              <UserRankingItem
-                key={display_name}
-                avatarUrl={avatar_url}
-                displayName={display_name}
-                index={index}
-                exp={50}
-              />
-            ))}
-          </tbody>
-        </table>
-      </main>
+      <ClientRankingPage />
     </div>
   );
 }
--- database.types.ts
+++ database.types.ts
@@ -11,6 +11,7 @@ export type Database = {
     Tables: {
       profiles: {
         Row: {
+          accepted_ranking: boolean
           avatar_url: string
           display_name: string
           email: string
@@ -18,6 +19,7 @@ export type Database = {
           updated_at: string
         }
         Insert: {
+          accepted_ranking?: boolean
           avatar_url?: string
           display_name?: string
           email: string
@@ -25,6 +27,7 @@ export type Database = {
           updated_at?: string
         }
         Update: {
+          accepted_ranking?: boolean
           avatar_url?: string
           display_name?: string
           email?: string

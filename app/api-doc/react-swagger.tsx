60001d8545ba3e73e42c6513883b7a20d589f6e6
Kodie <kodii.tulip@gmail.com>
2025-01-31 15:18:51 -0300

    fix: update de modulos

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -6,8 +6,13 @@ import 'swagger-ui-react/swagger-ui.css';
 type SwaggerProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   spec: Record<string, any>;
+  url: string | undefined;
 };
 
-export default function ReactSwagger({ spec }: SwaggerProps) {
-  return <SwaggerUI spec={spec} />;
+export default function ReactSwagger({ spec, url }: SwaggerProps) {
+  if (process.env.NODE_ENV === 'development') {
+    return <SwaggerUI spec={spec} />;
+  } else {
+    return <SwaggerUI url={url} />;
+  }
 }

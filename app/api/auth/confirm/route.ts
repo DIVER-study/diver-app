fa81dc854eb9de73540cb2818a6a80bb53ff7253
Kodie <kodii.tulip@gmail.com>
2025-02-06 21:30:24 -0300

    fix: remove swagger ui

--- app/api/auth/confirm/route.ts
+++ app/api/auth/confirm/route.ts
@@ -4,34 +4,6 @@ import { type NextRequest } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
-/**
- * @swagger
- * /api/auth/confirm:
- *  summary: Confirma usuário apartir de um token
- *  get:
- *    description: Rota utlizada para confirmação de ususário. Captura o token inserido no url, o tipo de otp e a página de redirecionamento
- *    parameters:
- *      - in: query
- *        name: token_hash
- *        schema:
- *          type: string
- *          required: true
- *          description: Token de uso único para autenticar o usuário
- *      - in: query
- *        name: type
- *        schema:
- *          type: string
- *          required: true
- *          enum: [email , signup , invite , magiclink , recovery , email_change]
- *          description: Tipo de autenticação
- *      - in: query
- *        name: next
- *        schema:
- *          type: string
- *          required: false
- *          default: '/'
- *          description: Site para redirecionar o usuário
- */
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');

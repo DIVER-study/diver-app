0d50e39a3ed38e9c6f7d5f49054d00fc9de9641b
pvictor1206 <paulo.magalhaes@darmlabs.ifce.edu.br>
2025-02-17 19:42:41 -0300

    fix: nao use img use Image from next/image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,24 @@
 import LoginForm from '@/components/LoginForm';
+import Image from 'next/image';
 
 export default function LoginPage() {
   return (
     <div className='flex items-center justify-center bg-[#F8F3E0] h-screen gap-15 flex-row'>
       <div className='flex flex-col items-center justify-center'>
-        <img
+        <Image
           src='/assets/loginImage.png'
           alt='Pesoas conversando'
-          className='w-130 object-cover' 
+          className='w-130 object-cover'
+          width={768}
+          height={768}
         />
-        <p className='text-center font-bold w-120 mt-[-5%]'><span className='text-[#FD7401]'>O conhecimento não surge do nada,</span> ele cresce no bate-papo, na troca de ideias e nas conexões que fazemos com os outros. É na interação social que aprendemos de verdade!</p>
+        <p className='text-center font-bold w-120 mt-[-5%]'>
+          <span className='text-[#FD7401]'>O conhecimento não surge do nada,</span> ele cresce no bate-papo, na troca de
+          ideias e nas conexões que fazemos com os outros. É na interação social que aprendemos de verdade!
+        </p>
       </div>
 
       <LoginForm />
     </div>
   );
 }
-

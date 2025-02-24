919f0c46accf514eee4b6bfd7bdc7dbf0928f9c8
eduarda_hey <cavalcantemeds@gmail.com>
2025-02-24 16:31:29 -0300

    Atualiza front da tela de conclusão

--- components/FinishingScreen.tsx
+++ components/FinishingScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from './ui/Button';
 import Link from 'next/link';
+import { FilledStar, FestiveCone } from '@/components/svgs/Svgs';
 
 type Props = { currentRealm: string; subjectId: number; xpDelta: number };
 
@@ -14,47 +15,38 @@ export const FinishingScreen = ({ currentRealm, subjectId, xpDelta }: Props) => 
   }, []);
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-white text-black p-6 absolute top-0 left-0 w-full'>
-      <h1
-        data-animate={animate}
-        className='text-3xl font-bold mb-6 transition-transform transform data-animate:scale-125'
-      >
-        PARABÉNS
-      </h1>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[#F8F3E0] text-black p-6 absolute top-0 left-0 w-full'>
+      <div className='flex gap-10 mt-[8rem]'>
+        <div>
+          <FestiveCone className='shrink-0 size-14 text-ranking-gold' />
+        </div>
+        <h1
+          data-animate={animate}
+          className='text-4xl font-bold mb-2 mt-4 transition-transform transform data-animate:scale-125 text-[#FD7401]'
+        >
+          Parabéns
+        </h1>
+        <div>
+          <FestiveCone className='shrink-0 size-14 text-ranking-gold rotate-270' />
+        </div>
+      </div>
+      
+      <p className='text-[#FD7401] text-xl mb-[2rem] font-medium'>Você concluiu esse exercício</p>
 
-      <div className='flex gap-4 mb-6'>
-        <div className='border-2 border-black rounded-lg px-8 py-4 text-xl transition-transform transform hover:scale-110'>
+      <div className='flex gap-5 mb-10 bg-white rounded-3xl w-[14rem] items-center justify-around'>
+        <div className='px-3 py-4 flex items-center justify-center transition-transform transform hover:scale-110'>
+          <span className='text-3xl'>
+            <FilledStar className='shrink-0 size-12 text-ranking-gold' />
+          </span>
+        </div>
+        <div className='px-4 py-4 text-xl transition-transform transform hover:scale-110 font-semibold'>
           + {xpDelta}XP
         </div>
-        <div className='border-2 border-black rounded-lg px-8 py-4 flex items-center justify-center transition-transform transform hover:scale-110'>
-          <span className='text-3xl'>🏆</span>
-        </div>
+        
       </div>
 
-      <div className='w-full max-w-md border-t-2 border-black pt-4'>
-        <div className='flex justify-between text-lg font-medium mb-2'>
-          <span>TEMAS PARA ESTUDO</span>
-          <span>ERRADAS</span>
-        </div>
-
-        <div className='flex justify-between'>
-          <div className='flex flex-col gap-2 w-3/4'>
-            <div className='h-4 bg-black w-full'></div>
-            <div className='h-4 bg-black w-full'></div>
-            <div className='h-4 bg-black w-full'></div>
-            <div className='h-4 bg-black w-full'></div>
-          </div>
-
-          <div className='flex flex-col gap-2 w-1/4 items-end'>
-            <div className='w-5 h-5 border-2 border-black rounded-full'></div>
-            <div className='w-5 h-5 border-2 border-black rounded-full'></div>
-            <div className='w-5 h-5 border-2 border-black rounded-full'></div>
-          </div>
-        </div>
-      </div>
-
-      <Button asChild>
-        <Link href={`/${currentRealm}/exerciseTrail?temaId=${subjectId}`}>Continuar</Link>
+      <Button asChild className='px-6 py-2 text-white text-base font-extrabold border-0 border-[#884E1D] border-solid border-b-4 self-end items-end mr-[12rem] mt-[8rem]'>
+        <Link href={`/${currentRealm}/exerciseTrail?temaId=${subjectId}`}>Concluir</Link>
       </Button>
     </div>
   );
